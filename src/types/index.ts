66984a43482acbb1b9cf490290dd5b7a0344203a
eddiee-s <edis.pajaziti@gmail.com>
2022-12-16 17:29:32 +0100

    created: scoreboard, dummy data/ finsh button:  ok

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface MatchDataTypes {
+    id: string;
+    homeTeam: string;
+    awayTeam: string;
+    initialHomeScore: number;
+    initialAwayScore: number;
+    updatedHomeScore: number;
+    updatedAwayScore: number;
+    gameStarted: boolean;
+    gameUpdated: boolean;
+    gameFinished: boolean;
+  }
